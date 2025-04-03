87b5c576b4b65428defc8e1b186e84be802e8101
picdilli <fraferrs35@icloud.com>
2025-04-03 21:00:24 +0200

    Initial Setup

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "@/app/components/Header";
+
+
+
+const TriviaPage = async () => {
+  return (
+    <main>
+      <Header />
+      <h1 className="text-2xl font-bold mb-4 p-4">Trivia Questions</h1>
+      
+    </main>
+  );
+};
+
+export default TriviaPage;
