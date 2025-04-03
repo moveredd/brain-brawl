696845e5861c4c4bb713ffe6aff173700763aff0
moveredd <fraferrs35@icloud.com>
2025-04-03 23:02:54 +0200

    Header changes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,10 +7,15 @@ const BFont = Bebas_Neue({ subsets: ['latin'], weight: '400' });
 
 const Header = () => {
   return (
-    <header className="relative z-50">
-      <nav className="bg-black h-[10rem] flex items-center justify-between px-10">
-        {/* Game Name on the Left */}
-        <h1 className={`text-white text-4xl ${BFont.className}`}>BrainBrawl</h1>
+    <>
+    <header className="relative z-50 ">
+      <nav className="bg-black h-[10rem] flex items-center justify-between px-10 bg-linear-to-t from-sky-500 to-indigo-500">
+                
+        <h1 className={`text-white text-4xl ${BFont.className}`}>
+          Brain
+          <span className='inline-block text-4xl'>🧠</span>
+          Brawl
+        </h1>
 
 
         <Points />
@@ -23,7 +28,8 @@ const Header = () => {
         </Link>
       </nav>
       <hr />
-    </header>
+    </header> 
+    </>
   );
 };
 
--- app/components/PointsCount.tsx
+++ app/components/PointsCount.tsx
@@ -8,7 +8,7 @@ const BFont = Bebas_Neue({ subsets: ['latin'], weight: '400' });
 const PointsCount = () => {
   return (
     <Link href="#">
-        <button className={`bg-blue-400 text-white px-6 py-3 rounded-lg shadow-md hover:bg-gray-200 bg-linear-to-r from-cyan-500 to-blue-500 transition text-3xl ${BFont.className}`}>
+        <button className={`bg-blue-400 text-white px-6 py-3 rounded-lg shadow-md hover:bg-gray-200 bg-linear-to-r from-cyan-600 to-blue-500 transition text-3xl border ${BFont.className}`}>
         Question <br/>
         1/10
         </button>
