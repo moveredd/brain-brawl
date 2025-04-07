2acdbc6944d3b49e9d18cc4f3957cb21ae1201b8
moveredd <fraferrs35@icloud.com>
2025-04-08 01:39:39 +0200

    Layout update

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-black">
+    <html lang="en" className="bg-black overscroll-none">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
