871411b2b8336268c44c8f3aa6a8a1362ac31df9
moveredd <fraferrs35@icloud.com>
2026-01-29 15:22:20 +0100

    feat: modified next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /**
-   * @type {boolean | 'error'}
-   * If true, Next.js will show an error if you try to use the pages directory
-   * when the app directory is detected.
-   */
-  experimental: {
-    appDir: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
+
